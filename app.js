6b5e6357dfce8abc21b131c01cc9d28bcc14f65c
fabio <fab.rmrz@gmail.com>
2017-08-14 01:20:48 -0500

    Create & Read added

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ const yargs = require("yargs");
 const notes = require("./notes.js");
 
 var argv = yargs.argv;
-var command = process.argv[2];
+var command = argv._[0];
 
 console.log(`Command: ${command}`);
 console.log('Yargs: ', argv);
 
 if (command === 'add') {
-	notes.addNote(argv.title, argv.body);
+	var note = notes.addNote(argv.title, argv.body);
+	if (note) {
+		console.log("Note created.");
+		notes.logNote(note);
+	} else {
+		console.log("Error - Title note in use");
+	}
 } else if (command === 'list') {
 	notes.getAll();
 } else if (command === 'read') {
-	notes.getNote(argv.title);
+	var note = notes.getNote(argv.title);
+	if (note) {
+		console.log("Note read.");
+		notes.logNote(note);	
+	} else {
+		console.log("Note not found");
+	}
 } else if (command === 'remove') {
-	notes.removeNote(argv.title);
+	var noteRemoved = notes.removeNote(argv.title);
+	var message = noteRemoved ? 'Note was removed' : 'Note not found';
+	console.log(message);
 }else {
 	console.log('Command not recognized');
 }
